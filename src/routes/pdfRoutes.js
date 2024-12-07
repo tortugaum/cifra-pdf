7d39107afff3a66e79467d9ed74ee2995b046dd6
tortugaum <luizfelipeforcato@gmail.com>
2024-12-07 12:00:59 -0300

    função tom, e busca de cifra add

--- src/routes/pdfRoutes.js
+++ src/routes/pdfRoutes.js
@@ -1,12 +1,15 @@
 const { isURL } = require('validator');
 const express = require('express');
-const { scrapeCifra } = require('../services/pupeteerScrapingService');
+const {
+  scrapeCifra,
+  scrapeSearchCifra,
+} = require('../services/pupeteerScrapingService');
 const pdfQueue = require('../queues/pdfQueue');
 
 const router = express.Router();
 
 router.post('/generate', async (req, res) => {
-  const { urls, name = null } = req.body; // Accepts an array of URLs
+  const { urls, name = null, tune = 0 } = req.body; // Accepts an array of URLs
 
   if (!urls || !Array.isArray(urls) || urls.length === 0) {
     return res
@@ -34,7 +37,7 @@ router.post('/generate', async (req, res) => {
 
     // Coletar as cifras de todas as URLs
     for (let url of urls) {
-      const cifraData = await scrapeCifra(url);
+      const cifraData = await scrapeCifra(url, tune);
       if (!cifraData) {
         return res
           .status(404)
@@ -54,6 +57,29 @@ router.post('/generate', async (req, res) => {
     console.error(error);
     res.status(500).json({ error: 'Erro ao gerar o PDF.' });
   }
+});
+
+router.get('/search', async (req, res) => {
+  const { artist, song } = req.query;
+
+  if (!artist || !song) {
+    return res.status(400).json({ error: 'Artist and song are required.' });
+  }
+
+  const songData = await scrapeSearchCifra(artist, song);
+
+  if (!songData) {
+    return res
+      .status(404)
+      .json({ error: `Cifra não encontrada para a URL: ${url}` });
+  }
+
+  const job = await pdfQueue.add({ pdfDataArray: [songData], name: null });
+
+  res.status(202).json({
+    message: 'Músicas adicionadas à fila para processamento.',
+    jobId: job.id,
+  });
 });
 
 module.exports = router;
--- src/services/pupeteerScrapingService.js
+++ src/services/pupeteerScrapingService.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 
 const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
 
-async function scrapeCifra(url) {
+async function scrapeCifra(url, tune) {
   try {
     // Launch Puppeteer
     const browser = await puppeteer.launch();
@@ -12,10 +12,16 @@ async function scrapeCifra(url) {
     // Navigate to the URL
     await page.goto(url, { waitUntil: 'domcontentloaded' });
 
-    await page.waitForResponse((response) => {
-      console.log(response.url());
-      return response.url().includes('transpose') && response.status() === 200; // Adjust this to match the network request URL or other criteria
-    });
+    if (url.includes('key=')) {
+      await waitForTranspose(page);
+    }
+
+    if (tune != 0) {
+      for (let i = 0; i < tune; i++) {
+        await page.click('#side-tom-more');
+      }
+      await waitForTranspose(page);
+    }
     // Extract the necessary data
     const title = await page.$eval('h1.t1', (el) => el.textContent.trim());
     const artist = await page.$eval('h2.t3', (el) => el.textContent.trim());
@@ -33,4 +39,58 @@ async function scrapeCifra(url) {
   }
 }
 
-module.exports = { scrapeCifra };
+const waitForTranspose = async (page) => {
+  await page.waitForResponse(
+    (response) => {
+      return response.url().includes('transpose') && response.status() === 200;
+    },
+    { timeout: 9000 }
+  );
+};
+
+async function scrapeSearchCifra(artist, song) {
+  try {
+    // Launch Puppeteer
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+
+    // Navigate to the Cifra Club website
+    await page.goto('https://www.cifraclub.com.br', {
+      waitUntil: 'domcontentloaded',
+    });
+
+    // Wait for the search field to be available
+    await page.waitForSelector('#js-h-search');
+
+    // Set the search field value to the artist and song
+    await page.type('#js-h-search', `${artist} ${song}`, { delay: 100 }); // Adding a delay for more natural typing
+
+    // Wait for suggestions to appear (wait for 500 ms after typing)
+    const suggestions = await page.evaluate(() => {
+      const suggestionElements = document.querySelectorAll('.list-suggest a'); // Select all <a> elements in the suggestions list
+      return Array.from(suggestionElements).map((el) => el.textContent.trim()); // Return the text content of each suggestion
+    });
+    // Click the first result under the list-suggest class
+    await page.click('.list-suggest a');
+
+    // Wait for the song page to load and the chords to appear
+    await page.waitForSelector('.cifra_cnt.g-fix.cifra-mono pre');
+
+    // Extract the song details
+    const title = await page.$eval('h1.t1', (el) => el.textContent.trim());
+    const artistName = await page.$eval('h2.t3', (el) => el.textContent.trim());
+    const cifra = await page.$eval('.cifra_cnt.g-fix.cifra-mono pre', (el) =>
+      el.textContent.trim()
+    );
+
+    // Close Puppeteer
+    await browser.close();
+
+    return { title, cifra, artist: artistName };
+  } catch (error) {
+    console.error('Erro ao acessar o site:', error);
+    return null;
+  }
+}
+
+module.exports = { scrapeCifra, scrapeSearchCifra };
